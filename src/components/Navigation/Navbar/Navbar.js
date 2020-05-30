73eb98026bd3d0b3caa6926cf54bfc5da533d782
ISNE501 <kanokpol.thongsem@gmail.com>
2020-05-30 23:07:49 +0700

    Add NavLink style

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import classes from './Navbar.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const navbar = (props) => (
     <header className={classes.Navbar}>
-        <div className={classes.Dashboard}><Link to="/">Dashboard</Link></div>
-        <div className={classes.Upload}><Link to={{pathname: "/upload"}}>Upload</Link></div>
-        <div className={classes.Projects}><Link to={{pathname: "/projects"}}>Projects</Link></div>
-        <div className={classes.Shared}><Link to={{pathname: "/sharedwithme"}}>Shared with me</Link></div>
+        <div className={classes.Dashboard}>
+            <NavLink to="/" exact activeStyle={{ color: 'black', textDecoration: 'underline' }}>Dashboard</NavLink>
+        </div>
+        <div className={classes.Upload}>
+            <NavLink to={{ pathname: "/upload" }} activeStyle={{ color: 'black', textDecoration: 'underline' }}>Upload</NavLink>
+        </div>
+        <div className={classes.Projects}>
+            <NavLink to={{ pathname: "/projects" }} activeStyle={{ color: 'black', textDecoration: 'underline' }}>Projects</NavLink>
+        </div>
+        <div className={classes.Shared}>
+            <NavLink to={{ pathname: "/sharedwithme" }} activeStyle={{ color: 'black', textDecoration: 'underline' }}>Shared with me</NavLink>
+        </div>
     </header>
 );
 
