0089d23f28b3d8949f71a63a4e85dba2608f5ed5
ISNE501 <kanokpol.thongsem@gmail.com>
2020-05-30 22:17:50 +0700

    Add Switch to route & Not found route

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <Layout></Layout>
-        </div>
+          <Layout />
       </BrowserRouter>
     );
   }
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Content.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Dashboard from '../../containers/Dashboard/Dashboard';
 import Upload from '../../containers/Upload/Upload';
 import Projects from '../../containers/Projects/Projects';
@@ -8,10 +8,13 @@ import Sharedwithme from '../../containers/Sharedwithme/Sharedwithme';
 
 const content = (props) => (
     <div className={classes.Content}>
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/upload" component={Upload} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/sharedwithme" component={Sharedwithme} />
+        <Switch>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/upload" component={Upload} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/sharedwithme" component={Sharedwithme} />
+            <Route render={()=><p>Error: Not found!</p>} />
+        </Switch>
     </div>
 );
 
