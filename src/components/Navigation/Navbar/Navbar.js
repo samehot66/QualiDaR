9e60b9d31ba49089a3b712dca9ef926ef084fb49
ISNE501 <kanokpol.thongsem@gmail.com>
2020-05-30 23:13:50 +0700

    Add bigger text when click

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -5,16 +5,16 @@ import { NavLink } from 'react-router-dom';
 const navbar = (props) => (
     <header className={classes.Navbar}>
         <div className={classes.Dashboard}>
-            <NavLink to="/" exact activeStyle={{ color: 'black', textDecoration: 'underline' }}>Dashboard</NavLink>
+            <NavLink to="/" exact activeStyle={{ color: '#036294', fontSize: '30px' }}>Dashboard</NavLink>
         </div>
         <div className={classes.Upload}>
-            <NavLink to={{ pathname: "/upload" }} activeStyle={{ color: 'black', textDecoration: 'underline' }}>Upload</NavLink>
+            <NavLink to={{ pathname: "/upload" }} activeStyle={{ color: '#036294', fontSize: '30px'}}>Upload</NavLink>
         </div>
         <div className={classes.Projects}>
-            <NavLink to={{ pathname: "/projects" }} activeStyle={{ color: 'black', textDecoration: 'underline' }}>Projects</NavLink>
+            <NavLink to={{ pathname: "/projects" }} activeStyle={{ color: '#036294', fontSize: '30px'}}>Projects</NavLink>
         </div>
         <div className={classes.Shared}>
-            <NavLink to={{ pathname: "/sharedwithme" }} activeStyle={{ color: 'black', textDecoration: 'underline' }}>Shared with me</NavLink>
+            <NavLink to={{ pathname: "/sharedwithme" }} activeStyle={{ color: '#036294', fontSize: '30px'}}>Shared with me</NavLink>
         </div>
     </header>
 );
