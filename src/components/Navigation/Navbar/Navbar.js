c5849c7d503cd3a4fd5fe6147118133d431b4902
ISNE501 <kanokpol.thongsem@gmail.com>
2020-05-31 00:41:33 +0700

    Add icon for navigation

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -3,20 +3,32 @@ import classes from './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
 const navbar = (props) => (
-    <header className={classes.Navbar}>
+    <nav className={classes.Navbar}>  
         <div className={classes.Dashboard}>
-            <NavLink to="/" exact activeStyle={{ color: '#036294', fontSize: '30px' }}>Dashboard</NavLink>
+            <NavLink to="/" exact activeStyle={{color: '#036294', fontSize: '27px', textDecoration: 'underline'}}>
+                <img className={classes.DashboardIcon} src={require('./icon/Dashboard.png')} />
+                Dashboard
+            </NavLink>
         </div>
         <div className={classes.Upload}>
-            <NavLink to={{ pathname: "/upload" }} activeStyle={{ color: '#036294', fontSize: '30px'}}>Upload</NavLink>
+            <NavLink to={{pathname: "/upload"}} activeStyle={{color: '#036294', fontSize: '28px', textDecoration: 'underline'}}>
+                 <img className={classes.UploadIcon} src={require('./icon/Upload.png')} /> 
+                 Upload
+            </NavLink>
         </div>
         <div className={classes.Projects}>
-            <NavLink to={{ pathname: "/projects" }} activeStyle={{ color: '#036294', fontSize: '30px'}}>Projects</NavLink>
+            <NavLink to={{pathname: "/projects"}} activeStyle={{ color: '#036294', fontSize: '28px', textDecoration: 'underline'}}>
+                <img className={classes.ProjectsIcon} src={require('./icon/Projects.png')} /> 
+                Projects
+            </NavLink>
         </div>
         <div className={classes.Shared}>
-            <NavLink to={{ pathname: "/sharedwithme" }} activeStyle={{ color: '#036294', fontSize: '30px'}}>Shared with me</NavLink>
+            <NavLink to={{pathname: "/sharedwithme"}} activeStyle={{color: '#036294', fontSize: '28px', textDecoration: 'underline'}}>
+            <img className={classes.SharedwithmeIcon} src={require('./icon/Sharedwithme.png')} />     
+                Shared with me
+            </NavLink>
         </div>
-    </header>
+    </nav>
 );
 
 export default navbar;
