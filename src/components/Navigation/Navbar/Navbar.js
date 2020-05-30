a3ae1095fd542acf70d6f8e72a29bba9097d8d96
ISNE501 <kanokpol.thongsem@gmail.com>
2020-05-30 20:10:05 +0700

    Add {Link} for without refresh page

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const navbar = (props) => (
     <header className={classes.Navbar}>
-        <div className={classes.Dashboard}><a href="/">Dashboard</a></div>
-        <div className={classes.Upload}><a href="/upload">Upload</a></div>
-        <div className={classes.Projects}><a href="/projects">Projects</a></div>
-        <div className={classes.Shared}><a href="/sharedwithme">Shared with me</a></div>
+        <div className={classes.Dashboard}><Link to="/">Dashboard</Link></div>
+        <div className={classes.Upload}><Link to={{pathname: "/upload"}}>Upload</Link></div>
+        <div className={classes.Projects}><Link to={{pathname: "/projects"}}>Projects</Link></div>
+        <div className={classes.Shared}><Link to={{pathname: "/sharedwithme"}}>Shared with me</Link></div>
     </header>
 );
 
