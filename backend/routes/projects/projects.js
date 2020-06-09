a4cd89389d6ebd4beefff22905334b02e66ccb72
purit1179 <purit_witeekon@cmu.ac.th>
2020-06-09 16:51:21 +0700

    update proejcts route

--- backend/routes/projects/projects.js
+++ backend/routes/projects/projects.js
@@ -2,16 +2,36 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../config/db.config.js');
 const Project = db.project
+const ProjectRole = db.project_role
 
-router.get('',(req,res) => {
-    Project.findAll({
+router.get('', async (req,res) => {
+    var projects = await Project.findAll({
         attributes : ['pid','name','num_subhead','num_text_component','description']
     }).then((data) => {
-        res.json(data)
+        return data
       }).catch((error) => {
         console.log(error)
         res.status(500).send(error)
       })
+      var projectOwner = await ProjectRole.findAll({
+        attributes : ['id','role','pid','uid'],
+        where: {
+          uid: req.body.uid
+        }
+      }).then((data) => {
+        //console.log(JSON.stringify(data))
+        return data
+      }).catch((error) => {
+        console.log(error)
+        res.status(500).send(error)
+      })
+      await console.log(projectOwner)
+      await console.log(projects)
+      var responseData = await {
+        projects: projects,
+        own: projectOwner
+      }
+      await res.json(responseData)     
 })
 
 module.exports = router
