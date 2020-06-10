d5b5841ad848310fdf510da346648997f9b68a63
purit1179 <purit_witeekon@cmu.ac.th>
2020-06-10 14:13:40 +0700

    fix GET projects route

--- backend/routes/projects/projects.js
+++ backend/routes/projects/projects.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const db = require('../../config/db.config.js');
 const Project = db.project
 const ProjectRole = db.project_role
+const User = db.user
 
 //GET
 //URL - http://localhost:4000/api/projects 
@@ -11,14 +12,19 @@ const ProjectRole = db.project_role
 //        }
 router.get('', async (req,res) => {
     var projects = await Project.findAll({
-        attributes : ['pid','name','num_subhead','num_text_component','description', 'createdAt', 'updatedAt']
+        attributes : ['pid','name','num_subhead','num_text_component','description', 'createdAt', 'updatedAt'],
+        include: [{
+          model: User,
+          where: {uid: req.body.uid},
+          attributes: ["uid", "email"]
+        }]
     }).then((data) => {
         return data
       }).catch((error) => {
         console.log(error)
         res.status(500).send(error)
       })
-      var projectOwner = await ProjectRole.findAll({
+      /*var projectOwner = await ProjectRole.findAll({
         attributes : ['id','role','pid','uid', 'createdAt', 'updatedAt'],
         where: {
           uid: req.body.uid
@@ -29,19 +35,19 @@ router.get('', async (req,res) => {
             message: "no own project"
           }
         }
-        //console.log(JSON.stringify(data))
+        console.log(JSON.stringify(data))
         return data
       }).catch((error) => {
         console.log(error)
         res.status(500).send(error)
       })
-      await console.log(projectOwner)
+      //await console.log(projectOwner)
       await console.log(projects)
       var responseData = await {
         projects: projects,
         own: projectOwner
-      }
-      await res.json(responseData)     
+      }*/
+      await res.json(projects)     
 })
 
 //POST
