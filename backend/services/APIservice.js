037dddc5084e7ba57afc72a07897fdef74bc2963
purit1179 <purit_witeekon@cmu.ac.th>
2020-06-10 17:03:53 +0700

    fix token param name

--- backend/services/APIservice.js
+++ backend/services/APIservice.js
@@ -3,7 +3,7 @@ var db = require('../config/db.config')
 module.exports = {getUserDetails: (userId, token) => {
     return new Promise((resolve, reject) => {
        db.user
-         .findOne( {where: { 'uid': userId, 'accessToken':  token}})
+         .findOne( {where: { 'uid': userId, 'access_token':  token}})
          .then((data) => {
             if(data){
                resolve(data);
--- backend/utils/APIauth.utils.js
+++ backend/utils/APIauth.utils.js
@@ -1,14 +1,14 @@
 var Client = require('../services/APIservice')
 
 module.exports = {clientApiKeyValidation: async (req,res,next) => {
-    if(!req.body.accessToken){
+    if(!req.body.access_token){
         return res.status(400).send({
            status:false,
            response:"Missing token!"
         });
      }
 try {
-      let clientDetails = await Client.getUserDetails(req.body.uid, req.body.accessToken);
+      let clientDetails = await Client.getUserDetails(req.body.uid, req.body.access_token);
       if (clientDetails) {
          next();
       }
