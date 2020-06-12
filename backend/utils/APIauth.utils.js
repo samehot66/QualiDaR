1b724801eb9343506f3e1ec7091cf51ae202b988
purit1179 <purit_witeekon@cmu.ac.th>
2020-06-12 14:26:59 +0700

    change params=>query

--- backend/utils/APIauth.utils.js
+++ backend/utils/APIauth.utils.js
@@ -2,14 +2,14 @@ var Client = require('../services/APIservice')
 
 module.exports = {clientApiKeyValidation: async (req,res,next) => {
    //console.log(req)
-    if(!req.params.access_token){
+    if(!req.query.access_token){
         return res.status(400).send({
            status:false,
            response:"Missing token!"
         });
      }
 try {
-      let clientDetails = await Client.getUserDetails(req.params.uid, req.params.access_token);
+      let clientDetails = await Client.getUserDetails(req.query.uid, req.query.access_token);
       if (clientDetails) {
          next();
       }
