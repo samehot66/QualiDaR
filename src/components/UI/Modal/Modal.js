34bd19677b87f229a944e0eedbcc11b97d17ba16
ISNE501 <kanokpol.thongsem@gmail.com>
2020-07-27 00:11:26 +0700

    Use props to modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -10,28 +10,28 @@ const modal = (props) => {
         <Auxi>
             <Blackdrop show={props.show} clicked={props.modalClosed} />
             <div className={classes.Modal} onClick={props.clicked} style={{
-                    transfrom: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0',
-                    display: props.show ? 'block' : 'none'
-                }}>
-            <div className="card card-primary" >
-  <div className="card-header">
-    <h3 className="card-title">Title</h3>
-    <div className="card-tools">
-      <button type="button" className="btn btn-tool" onClick={props.modalClosed}>
-        <i className="fas fa-times" /></button>
-    </div>
-  </div>
-  <div className="card-body text-wrap" style={{display: 'block'}}>
-    Start creating your amazing applicationxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx!
-  </div>
-  {/* /.card-body */}
-  <div className="card-footer" style={{display: 'block'}}>
-    Footer
-  </div>
-  {/* /.card-footer*/}
-</div>
-</div>
+                transfrom: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0',
+                display: props.show ? 'block' : 'none'
+            }}>
+                <div className="card card-info" >
+                    <div className="card-header">
+                        <h3 className="card-title">{props.name}</h3>
+                        <div className="card-tools">
+                            <button type="button" className="btn btn-tool" onClick={props.modalClosed}>
+                                <i className="fas fa-times" /></button>
+                        </div>
+                    </div>
+                    <div className="card-body text-wrap" >
+                        {props.children}
+                    </div>
+                    {/* /.card-body */}
+                    <div className="card-footer">
+                        Footer
+                    </div>
+                    {/* /.card-footer*/}
+                </div>
+            </div>
         </Auxi>
     )
 };
