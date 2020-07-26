d4cd649bc0770b314d12cf419cb2159eaabb6ae7
ISNE501 <kanokpol.thongsem@gmail.com>
2020-07-26 21:39:27 +0700

    There is not session -> hide home button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-
+    const [isauth,setisauth] =  useState(localStorage.getItem('isAuth')); 
+    
     return (
         <nav className="main-header navbar navbar-expand navbar-white navbar-light">
             {/* Left navbar links */}
@@ -10,9 +11,10 @@ const Header = () => {
                 <li className="nav-item">
                     <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars" /></a>
                 </li>
+                { isauth ?
                 <li className="nav-item d-none d-sm-inline-block">
                     <NavLink to="/dashboard" className="nav-link">Home</NavLink>
-                </li>
+                </li>:null}
             </ul>
            
             {/* Right navbar links */}
