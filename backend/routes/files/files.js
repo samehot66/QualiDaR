5d7f9610d0a79862070d0edeb885a6dd11ffc34b
purit1179 <purit_witeekon@cmu.ac.th>
2020-07-26 17:44:40 +0700

    create perform task after recieve a file

--- backend/routes/files/files.js
+++ backend/routes/files/files.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios');
 
-router.post('/upload', (req, res) => {
+router.post('/upload', async (req, res, next) => {
     console.log(`first`)
     if (req.files === null) {
       return res.status(400).json({ msg: 'No file uploaded' });
@@ -15,8 +16,26 @@ router.post('/upload', (req, res) => {
         return res.status(500).send(err);
       }
   
-      res.json({ fileName: file.name, filePath: `/QBDRRs/backend/public/uploads/${file.name}` });
+      res.json({ fileName: file.name, filePath: `/QBDRRs/backend/public/uploads/${file.name}` })
+      performTask(file.name)
+      //.then(()=>{
+        
+      /*})
+      .catch((err)=>{
+        res.status(500).send(err)
+      });*/
     });
   });
+
+  performTask = (fileName) => {
+    axios.post("http://localhost:5000/task", {
+        file: `D:/Project/QBRRS/QBDRRs/backend/public/upload/${fileName}`
+    }).then((res) => {
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(res)
+      }).catch((err)=>{
+          console.log(err)
+      })
+  }
 
 module.exports = router
